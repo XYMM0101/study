f455068e15023d428fc253839c0babcefe72dde1
YXMM0101 <YXMM0101@163.com>
2019-06-17 20:05:19 +0800

    xiugaibianliang

--- ceshi.sh
+++ ceshi.sh
@@ -1,7 +1,7 @@
 #/usr/bin/env bash
-str01=wangning shi zhu !
+var01=wangning shi zhu !
 <<<<<<< HEAD
-str02=var01.replace('w','W')
+var02=var01.replace('w','W')
 print(str02)
 =======
 var02=var01.replace('w','W')
